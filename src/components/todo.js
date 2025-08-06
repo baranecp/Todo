4d4542bc68335e43033dc328c66f12f1bb17f33e
baranecp <baranec.dev@gmail.com>
2025-08-06 12:01:33 +0200

    Todo class

---
+++ src/components/todo.js
@@ -0,0 +1,52 @@
+export default class Todo {
+  constructor({
+    title,
+    description,
+    dueDate,
+    priority,
+    id = crypto.randomUUID(),
+    isCompleted = false,
+    isEditing = false,
+  }) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = new Date(dueDate);
+    this.priority = priority;
+    this.id = id;
+    this.isCompleted = isCompleted;
+    this.isEditing = isEditing;
+  }
+
+  toggleCompleted() {
+    this.isCompleted = !this.isCompleted;
+  }
+
+  toggleEditting() {
+    this.toggleEditting = !this.isEditing;
+  }
+
+  updateTitle(newTitle) {
+    this.title = newTitle;
+  }
+
+  updateDescription(newDescription) {
+    this.description = newDescription;
+  }
+
+  updateDueDate(newDate) {
+    const date = new Date(newDate);
+    this.dueDate = date;
+  }
+
+  updatePriorityLevel(newPriority) {
+    this.priority = newPriority;
+  }
+
+  get summary() {
+    return `${this.title} - Due: ${this.dueDate?.toLocaleDateString()}`;
+  }
+
+  set priorityLevel(level) {
+    this.priority = level;
+  }
+}
